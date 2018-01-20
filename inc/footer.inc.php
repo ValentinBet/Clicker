037a9580974eee6cef1f858da307965f15a6592c
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2018-01-20 21:12:35 +0100

    origine

---
+++ inc/footer.inc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Betrancourt Valentin
+ * Date: 20/01/2018
+ * Time: 19:38
+ */
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+require 'inc/header.inc.php';
+?>
+
+
+
+<?php
+
+
+require 'inc/footer.inc.php';
+/**
+ * Created by PhpStorm.
+ * User: Betrancourt Valentin
+ * Date: 20/01/2018
+ * Time: 19:37
+ */
+?>
